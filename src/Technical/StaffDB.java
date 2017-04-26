e6d2b3b938dca4c056d46ca02d88d5d604ddbfe9
lordr3born <lpnielsen@hotmail.com>
2017-04-26 18:51:13 +0200

    Removed Sout's

--- src/Technical/StaffDB.java
+++ src/Technical/StaffDB.java
@@ -7,15 +7,13 @@ import javax.swing.*;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Martynas on 2017-04-24.
  */
 public class StaffDB extends DBFacade {
-    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-mm-dd");
-    private static LocalDate Date = LocalDate.now();
-    private static String localDate = dtf.format(Date);
+    java.sql.Date d;
+    LocalDate localDate = LocalDate.now();
     private String staff_FName;
     private String staff_LName;
     private String staff_PhoneNo;
@@ -27,46 +25,45 @@ public class StaffDB extends DBFacade {
     private static String username;
     private static String password;
 
-    public void createStaffMember(String staff_FName, String staff_LName,
-                                  String staff_PhoneNo, String staff_Mail,
-                                  String staff_Address, int city_Zip, String jobtitle_Occupation) throws SQLException{
-        this.staff_FName = staff_FName;
-        this.staff_LName = staff_LName;
-        this.staff_PhoneNo = staff_PhoneNo;
-        this.staff_Mail = staff_Mail;
-        this.staff_Address = staff_Address;
-        this.city_Zip = city_Zip;
-        this.jobtitle_Occupation = jobtitle_Occupation;
+    public void createStaffMember(String FName, String LName,
+                                  String PhoneNo, String Mail,
+                                  String Address, int Zip, String Occupation) throws SQLException{
+        this.staff_FName = FName;
+        this.staff_LName = LName;
+        this.staff_PhoneNo = PhoneNo;
+        this.staff_Mail = Mail;
+        this.staff_Address = Address;
+        this.city_Zip = Zip;
+        this.jobtitle_Occupation = Occupation;
 
         cs = con.prepareCall("{call CreateStaffMember(?,?,?,?,?,?,?,?)}");
         cs.setString(1,staff_FName);
         cs.setString(2,staff_LName);
         cs.setString(3,staff_PhoneNo);
         cs.setString(4,staff_Mail);
-        cs.setString(5,localDate);
+        cs.setString(5, localDate.toString());
         cs.setString(6,staff_Address);
         cs.setInt(7,city_Zip);
         cs.setString(8,jobtitle_Occupation);
         cs.execute();
 
-        generateAccount();
     }
 
-    private void generateAccount() throws SQLException {
-        cs = con.prepareCall("{call CreateAccount(?,?,?)}");
-        cs.setString(1, Account.getUserName());
-        cs.setString(2, Account.getPassword());
-        cs.setString(4, Account.getUserLevel());
-        cs.execute();
-        cs.close();
-    }
-
-    public Account createStaffAccount(JTextField fname,JTextField lname,JComboBox comboBox) {
+    public Account createStaffAccount(JTextField fname,JTextField lname,JComboBox comboBox) throws SQLException {
         Account.setUserName(fname.getText(),lname.getText());
         Account.setPassword();
         username = Account.getUserName();
         password = Account.getPassword();
         return Creator.createStaffAccount(username,password,comboBox.getSelectedItem().toString());
+    }
+
+    public void generateAccount() throws SQLException {
+        cs = con.prepareCall("{call CreateAccount(?,?,?)}");
+        cs.setString(1, Account.getUserName());
+        cs.setString(2, Account.getPassword());
+        cs.setString(3, Account.getUserLevel());
+        cs.execute();
+        cs.close();
     }
 
     public void deleteStaffMember (int staff_ID) throws SQLException{

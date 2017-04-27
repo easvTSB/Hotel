65e3ef4365c05e6ca970869c23cbd91a8b3180b6
Lasse <lpnielsen@hotmail.com>
2017-04-27 10:44:52 +0200

    Recreated Staff Object

--- src/Domain/Staff.java
+++ src/Domain/Staff.java
@@ -10,23 +10,99 @@ import java.time.LocalDate;
  * Created by LPNielsen on 25-Apr-17.
  */
 public class Staff {
-    private static int number = 8;
-    private static String fName;
-    private static String lName;
-    private static String phoneNo;
-    private static String email;
-    private static String address;
-    private static int zipCode;
-    private static String jobTitle;
+    private int ID;
+    private String fName;
+    private String lName;
+    private String phoneNo;
+    private String email;
+    private String address;
+    private int zipCode;
+    private String jobTitle;
 
     public Staff(String fName, String lName, String phoneNo, String email,
                  String address, int zipCode, String jobTitle) {
-        Staff.fName = fName;
-        Staff.lName = lName;
-        Staff.phoneNo = phoneNo;
-        Staff.email = email;
-        Staff.address = address;
-        Staff.zipCode = zipCode;
-        Staff.jobTitle = jobTitle;
+        this.fName = fName;
+        this.lName = lName;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.jobTitle = jobTitle;
+    }
+
+    public Staff(int staff_ID, String fName, String lName, String phoneNo, String email,
+                 String address, int zipCode, String jobTitle) {
+        this.ID = staff_ID;
+        this.fName = fName;
+        this.lName = lName;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
     }
 }
